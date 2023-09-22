9d8f902504aee0f2183cf1e50c71ab4c72c89066
bread <chalida.bread@gmail.com>
2023-09-22 22:09:39 +0700

    fix : debugging to resolve an issue

---
+++ assignment-type-convension-chalida/ex-1.js
@@ -0,0 +1,10 @@
+// Exercise #1: Debugging
+// 
+let accountBalance = 400;
+let depositAmountFromUser = "10";
+
+accountBalance = accountBalance + depositAmountFromUser;
+
+let accountBalanceMessage = `Your account balance is ${accountBalance}`;
+
+console.log(accountBalanceMessage);
